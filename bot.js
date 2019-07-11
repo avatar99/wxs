55ed358f3e54b399573fcfb637f3b64926bda894
avatar99 <51578188+avatar99@users.noreply.github.com>
2019-07-11 15:34:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ client.on('message' , message => {//bcrole
     }
 });
 
-const developers = ["557181389100023839","id"]
+const developers = ["458371307525111809","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
